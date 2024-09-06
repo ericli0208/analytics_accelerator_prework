02120e60df0a93ae4ef117c9a800d5e73523b9c9
Eric Li <ericli0208@gmail.com>
2024-09-06 16:43:44 -0700

    Update udacity_course_4.sql

--- udacity_course_4.sql
+++ udacity_course_4.sql
@@ -31,3 +31,39 @@ This is because the subquery is treated as an individual value (or set of values
 Also, notice the query here compared a single value. 
 If we returned an entire column IN would need to be used to perform a logical argument. 
 If we are returning an entire table, then we must use an ALIAS for the table, and perform additional logic on the entire table. */ 
+
+/* Questions for Subqueries 
+1. Provide the name of the sales_rep in each region with the largest amount of total_amt_usd sales.
+2. For the region with the largest (sum) of sales total_amt_usd, how many total (count) orders were placed?
+3. How many accounts had more total purchases 
+than the account name which has bought the most standard_qty paper throughout their lifetime as a customer?
+4. For the customer that spent the most (in total over their lifetime as a customer) total_amt_usd, 
+how many web_events did they have for each channel?
+5. What is the lifetime average amount spent in terms of total_amt_usd for the top 10 total spending accounts?
+6. What is the lifetime average amount spent in terms of total_amt_usd, 
+including only the companies that spent more per order, on average, than the average of all orders. */ 
+
+SELECT t3.rep_name, t3.region_name, t3.total_amt
+FROM(SELECT region_name, MAX(total_amt) total_amt
+        FROM(SELECT s.name rep_name, r.name region_name, SUM(o.total_amt_usd) total_amt
+                FROM sales_reps s
+                JOIN accounts a
+                ON a.sales_rep_id = s.id
+                JOIN orders o
+                ON o.account_id = a.id
+                JOIN region r
+                ON r.id = s.region_id
+                GROUP BY 1, 2) t1
+        GROUP BY 1) t2
+JOIN (SELECT s.name rep_name, r.name region_name, SUM(o.total_amt_usd) total_amt
+        FROM sales_reps s
+        JOIN accounts a
+        ON a.sales_rep_id = s.id
+        JOIN orders o
+        ON o.account_id = a.id
+        JOIN region r
+        ON r.id = s.region_id
+        GROUP BY 1,2
+        ORDER BY 3 DESC) t3
+ON t3.region_name = t2.region_name AND t3.total_amt = t2.total_amt;
+
